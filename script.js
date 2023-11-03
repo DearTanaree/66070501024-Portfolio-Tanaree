4e1a9d8ba5fc73eab2f17d6d13154cc38e75b1ed
DearTanaree <103647056+DearTanaree@users.noreply.github.com>
2023-11-04 00:24:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let section = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(se =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHright;
+        let id = sec.getAttribute('id');
+    });
+};
+
